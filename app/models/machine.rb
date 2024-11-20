f064d1ab8063da3ea2d886ef1273c335d9f5b8f9
Marco Castro <marcodotcastro@gmail.com>
2024-11-20 20:51:19 -0300

    feat: validate machine

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -9,9 +9,18 @@ class Machine < ApplicationRecord
   validates :code, presence: true, uniqueness: true
   validates :name, presence: true
   validates :status, presence: true
+  validate :validate_code_format
 
   # Scopes
   scope :search, ->(term) {
     where("code ILIKE :term OR name ILIKE :term", term: "%#{term}%") if term.present?
   }
+
+  private
+
+  def validate_code_format
+    unless code.match?(/\A[A-Z][a-z]{2}\d{3}\z/)
+      errors.add(:code, "deve seguir o formato. Ex: Cen006, Dob020")
+    end
+  end
 end
