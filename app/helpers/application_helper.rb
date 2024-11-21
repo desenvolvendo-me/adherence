356dc6b51a136c70c03fcc2f15723035e39de8b2
Marco Castro <marcodotcastro@gmail.com>
2024-11-20 22:13:15 -0300

    fix: pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def page_entries_info(collection)
+    return unless collection.total_pages > 1
+
+    start_count = (collection.current_page - 1) * collection.limit_value + 1
+    end_count = [start_count + collection.limit_value - 1, collection.total_count].min
+
+    "Mostrando #{start_count} até #{end_count} de #{collection.total_count} registros"
+  end
+
 end
